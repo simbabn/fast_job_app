b5059c5d69d293177e3ddfb560d8ba223977b0f7
nazimb <nazim.bensouiki@converteo.com>
2023-10-16 14:14:05 +0200

    initial commit

--- fast_job/interface/models.py
+++ fast_job/interface/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    intitule = models.CharField(max_length=150)
+    description = models.CharField(max_length=1000)
+    entreprise = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    lieu = models.CharField(max_length=100)
+    type_contrat = models.CharField(max_length=5)
+    experience = models.CharField(max_length=100)
+    exigences = models.TextField()
+    qualification = models.CharField(max_length=100)
+    qualites_pro = models.TextField()
+    formations = models.CharField(max_length=100)
+    langues = models.CharField(max_length=100)
+    salaire = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
+
+	
+def __str__(self):
+	return(f"{self.intitule} {self.entreprise}")
